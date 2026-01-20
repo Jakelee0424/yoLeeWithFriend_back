485d5cac6ee14c82aff2cdc0749138c8770626c7
gamewithfriend <jse3685545@naver.com>
2026-01-20 21:33:43 +0900

    [유저] 마이페이지 리뷰 커밋

--- src/main/java/klj/project/web/dto/user/board/CommentResDto.java
+++ src/main/java/klj/project/web/dto/user/board/CommentResDto.java
@@ -1,8 +1,11 @@
 package klj.project.web.dto.user.board;
 
 import klj.project.domain.board.Comment;
+import klj.project.web.dto.admin.board.BoardMngrResDto;
 import klj.project.web.dto.user.user.UserInfoResponseDto;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -17,16 +20,20 @@ public class CommentResDto {
     private Integer tasteRate;
     private Integer priceRate;
     private Integer ingredientRate;
+    private BoardMngrResDto board;
+    private LocalDateTime regDt;
 
     public static CommentResDto fromEntity(Comment comment) {
         return CommentResDto.builder()
                 .commentId(comment.getCommentId())
                 .boardId(comment.getBoard() != null ? comment.getBoard().getBoardId() : null)
                 .user(comment.getUser() != null ? comment.getUser().toResponseDto() : null)
+                .board(comment.getBoard() !=null ? comment.getBoard().toResponseDto() : null)
                 .content(comment.getContent())
                 .tasteRate(comment.getTasteRate())
                 .priceRate(comment.getPriceRate())
                 .ingredientRate(comment.getIngredientRate())
+                .regDt(comment.getRegDt())
                 .build();
     }
 }
