1605bf69d153c4489f4f52563b5d36306fbf1b2e
gamewithfriend <jse3685545@naver.com>
2026-01-21 21:50:17 +0900

    [유저] 마이페이지 리뷰 및 로그 보드 커밋

--- src/main/java/klj/project/repository/util/LogsUserQuerydslRepository.java
+++ src/main/java/klj/project/repository/util/LogsUserQuerydslRepository.java
@@ -5,6 +5,7 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import klj.project.domain.board.QBoard;
+import klj.project.domain.file.QFiles;
 import klj.project.domain.util.QLogs;
 import klj.project.domain.util.QLogsUser;
 import klj.project.web.dto.admin.board.BoardMngrResDto;
@@ -122,7 +123,13 @@ public class LogsUserQuerydslRepository {
 
         List<BoardMngrResDto> logsList = queryFactory
                 .select(Projections.fields(BoardMngrResDto.class,
-                        QBoard.board
+                        QBoard.board.boardName,
+                        QBoard.board.boardId,
+                        QBoard.board.boardCategoryCodeId,
+                        QBoard.board.createDate,
+                        QBoard.board.brandCodeId,
+                        QBoard.board.fileGroupId,
+                        QFiles.files.filePath.as("imgUrl")
                 )).from(QLogsUser.logsUser)
                 .leftJoin(QBoard.board)
                 .on(QBoard.board.boardId.stringValue().eq(
@@ -133,9 +140,12 @@ public class LogsUserQuerydslRepository {
                                 ""
                         )
                 ))
+                .leftJoin(QFiles.files).on(QFiles.files.fileGroup.id.eq(QBoard.board.fileGroupId))
                 .where(
-                        QLogsUser.logsUser.userId.eq(userId)
+                        QLogsUser.logsUser.userId.eq(userId),
+                        QLogsUser.logsUser.description.ne("")
                 )
+                .limit(5)
                 .orderBy(QLogsUser.logsUser.createdDate.desc(),QLogsUser.logsUser.id.desc())
                 .fetch();
 
